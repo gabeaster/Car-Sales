7c071a0cab4b301646d291c62622efbddb69d162
gabeaster <gab.easter@live.com>
2020-04-14 17:25:09 -0400

    buyItem action created in action.js

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -6,3 +6,11 @@ export const removeFeature = item => {
     };
 };
 
+export const buyItem = item => {
+    console.log(item)
+    return {
+        type: "BUY_ITEM", 
+        payload: item
+    };
+};
+
